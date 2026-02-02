b2d7d11dd4533c0114a28ffdd04fd926913d31a8
dncworking <dominyka.dobuzinskaite@gmail.com>
2026-02-02 14:49:27 +0200

    added data to sportsData.json

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+import express from 'express';
 
+const app = express();
+
+
+app.use(express.json());
+
+
+export default app;
---
+++ data/sportsData.js
@@ -0,0 +1,8 @@
+export const sports = [
+  {
+    id: 1,
+    name: "Football",
+    popularityRank: 1,
+    players: [{ id: "101", name: "John Doe", age: 25, position: "Forward" }],
+  },
+];
